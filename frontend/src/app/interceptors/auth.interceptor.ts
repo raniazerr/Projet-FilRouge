70dfd34e0be17e2c815443701a246c41548cefba
raniazerr <rania0270@outlook.fr>
2026-08-02 16:00:08 +0200

    🔒️ Mise en place d'un intercepteur de tokens 🔒️

---
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/AuthService';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
+};
